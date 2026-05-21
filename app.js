c132f957c01864858d9f709cee8cacdb0ec4c3ec
PrincesMargarethLatiza <PrincesMargarethLatiza>
2026-05-21 21:28:10 +0800

    Confirm import 'dotenv/config', express, cors, connectDB and notificationRoutes with named limiter as logsLimiter are present.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+import notificationRoutes, { limiter as logsLimiter } from './routers/notificationRoutes.js';
+
 
 export default app;
